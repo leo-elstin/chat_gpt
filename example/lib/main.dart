a08c4aebd86916d382cf6aa545beeb4b99f9e376
Leo Elstin <leoelstin@gmail.com>
2023-01-13 17:17:39 +0530

    example added

---
+++ example/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:chat_gpt/chat_gpt.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final chatGpt = ChatGPT.builder(
+    token:
+        '<TOKEN>', // generate token from https://beta.openai.com/account/api-keys
+  );
+
+  void textCompletion() async {
+    const prompt = "Explain Quantum Computing in simple terms";
+
+    Completion? completion = await chatGpt.textCompletion(
+      request: const CompletionRequest(
+        prompt: prompt,
+        maxTokens: 256,
+      ),
+    );
+
+    print(completion?.choices);
+
+    Images? images = await chatGpt.generateImage(
+      request: const ImageRequest(
+        prompt: 'A cute baby sea otter',
+      ),
+    );
+
+    print(images?.data);
+  }
+
+  void imageGeneration() async {
+    Images? images = await chatGpt.generateImage(
+      request: const ImageRequest(
+        prompt: 'A cute baby sea otter',
+      ),
+    );
+
+    print(images?.data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '0',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: textCompletion,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
