5dd01c85977c809d766fade3aaa3d4244c2d9d2f
Muhammad Mohiuddin <muhammad.mohiuddin@live.com>
2023-10-15 02:25:37 +0200

    reformat

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -43,7 +43,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final chatGpt = ChatGPT.builder(
     token:
-    '<TOKEN>', // generate token from https://beta.openai.com/account/api-keys
+        '<TOKEN>', // generate token from https://beta.openai.com/account/api-keys
   );
 
   void textCompletion() async {
